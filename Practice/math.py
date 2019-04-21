3cf3944eef7e7e019f42647382390c5f944e4917
BBQ20190420 <baoqianqq@163.com>
2019-04-21 20:36:14 +0800

    测试提交

---
+++ Practice/math.py
@@ -0,0 +1,86 @@
+#a=int(input("please input one number:"))
+
+# if a > 0:
+#     a+=1
+#     print(a)
+# else:
+#     a-=1
+#     print(a)
+
+# while a!=None:
+#     if a>0:
+#         print("successful")
+#         break
+#     else:
+#         print("error")
+#         continue
+
+
+# n = 8
+# while n > 0:
+#     a = int(input("please input one number:"))
+#
+#     if a >= 90:
+#         print("level is A")
+#     elif a >= 60:
+#         print("level is B")
+#     elif a >= 0:
+#         print("level is E")
+#     else:
+#         print("error")
+#
+#     n = n - 1
+
+# i=1
+# sum=0
+# while i<=100:
+#     sum=sum+i
+#     i=i+1
+# print(sum)
+
+
+# for i in range(1,1001):
+#     if i%2!=0:
+#         print(i)
+
+# for i in range(2,10):
+#     for j in range(3,i):
+#         if i%j==0:
+#             break
+
+#10的阶乘
+# result = 1
+# i = 1
+# while i <= 10:
+#     result = result * i
+#     i += 1
+# print(result)
+
+#被3整除
+# list=[]
+# for i in range(1,101):
+#     if i%3==0:
+#         list.append(i)
+# print(list)
+
+list1=[2]
+
+#1000内的质数
+for i in range(3,1001):
+    for j in range(2,i):
+        if i%j==0:
+            break
+        list1.append(i)
+
+print(list(set(list1)))
+
+#兔子
+a = 1
+b = 2
+n=1
+while n <=10:
+    print(a)
+    c = a + b
+    a = b
+    b = c
+    n += 10
